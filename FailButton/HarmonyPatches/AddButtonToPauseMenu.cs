64dad786d70492a16217cb902c8a7a4d117b4d45
qe201020335 <qe201020335@sina.com>
2024-11-06 23:16:52 -0500

    Add FailButton source
    Thank you Kinsi!

---
+++ FailButton/HarmonyPatches/AddButtonToPauseMenu.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using HMUI;
+using Polyglot;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using IPA.Utilities;
+using System.Collections;
+
+namespace FailButton.HarmonyPatches {
+	[HarmonyPatch(typeof(PauseMenuManager), "Start")]
+	static class AddButtonToPauseMenu {
+		static Transform _b = null;
+
+		static void Postfix(PauseMenuManager __instance) {
+			SharedCoroutineStarter.instance.StartCoroutine(DoTheFunny(__instance));
+		}
+
+		static IEnumerator DoTheFunny(PauseMenuManager __instance) {
+			if(_b != null)
+				yield break;
+
+			yield return null;
+
+			var c = __instance.transform.Find("Wrapper/MenuWrapper/Canvas/MainBar/Buttons");
+
+			if(c == null || c.parent == null || c.childCount == 0 || !c.GetChild(0).gameObject.activeInHierarchy)
+				yield break;
+
+			_b = GameObject.Instantiate(c.GetChild(0), c.parent.GetChild(0));
+			GameObject.Destroy(_b.GetComponentInChildren<LocalizedTextMeshProUGUI>());
+
+			var r = _b.GetComponent<RectTransform>();
+			r.anchorMin = new Vector2(0.53f, 0);
+			r.anchorMax = new Vector2(0.64f, 0.5f);
+
+			_b.localPosition = new Vector3(48.69f, -2.6f, 0);
+
+			var t = _b.GetComponentInChildren<TextMeshProUGUI>();
+			t.text = "Fail";
+
+			GameObject.DestroyImmediate(t.transform.parent.GetComponent<LayoutElement>());
+			GameObject.DestroyImmediate(t.transform.parent.GetComponent<StackLayoutGroup>());
+
+			var b = _b.GetComponent<NoTransitionsButton>();
+			b.onClick.RemoveAllListeners();
+
+			b.onClick.AddListener(() => {
+				var s = Resources.FindObjectsOfTypeAll<StandardLevelGameplayManager>().FirstOrDefault();
+
+				if(s == null)
+					return;
+
+				ReflectionUtil.GetField<StandardLevelGameplayManager.InitData, StandardLevelGameplayManager>(s, "_initData").SetField("failOn0Energy", true);
+
+				AccessTools.Method(typeof(StandardLevelGameplayManager), "HandleGameEnergyDidReach0").Invoke(s, null);
+			});
+
+			yield return null;
+
+			r = t.GetComponentInParent<RectTransform>();
+
+			r.anchorMin = new Vector2(1, 0);
+			r.anchorMax = new Vector2(0, 1);
+
+			r.offsetMin = r.offsetMax = Vector2.zero;
+		}
+	}
+}
--- FailButton/Plugin.cs
+++ FailButton/Plugin.cs
@@ -1,35 +1,50 @@
-﻿using IPA;
+﻿using HarmonyLib;
+using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using IPALogger = IPA.Logging.Logger;
 
-namespace FailButton
-{
-    [Plugin(RuntimeOptions.SingleStartInit)]
-    public class Plugin
-    {
-        internal static Plugin Instance { get; private set; }
-        /// <summary>
-        /// Use to send log messages through BSIPA.
-        /// </summary>
-        internal static IPALogger Log { get; private set; }
+namespace FailButton {
+	[Plugin(RuntimeOptions.SingleStartInit)]
+	public class Plugin {
+		internal static Plugin Instance { get; private set; }
+		internal static IPALogger Log { get; private set; }
+		internal static Harmony harmony { get; private set; }
 
+		[Init]
+		public void Init(IPALogger logger) {
+			Instance = this;
+			Log = logger;
+		}
+
+		#region BSIPA Config
+		//Uncomment to use BSIPA's config
+		/*
         [Init]
-        public Plugin(IPALogger logger)
+        public void InitWithConfig(Config conf)
         {
-            Instance = this;
-            Log = logger;
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            Log.Debug("Config loaded");
         }
+        */
+		#endregion
 
-        [OnStart]
-        public void OnApplicationStart()
-        {
-            Plugin.Log.Info("OnApplicationStart");
-        }
+		[OnStart]
+		public void OnApplicationStart() {
+			harmony = new Harmony("Kinsi55.BeatSaber.FailButton");
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
 
-        [OnExit]
-        public void OnApplicationQuit()
-        {
-
-        }
-
-    }
+		[OnExit]
+		public void OnApplicationQuit() {
+			harmony.UnpatchSelf();
+		}
+	}
 }
