c72cf3fb19fcd00a25ef6c151b1ec750fa16fac6
qe201020335 <qe201020335@sina.com>
2024-11-07 00:21:17 -0500

    Reformat code

--- FailButton/Plugin.cs
+++ FailButton/Plugin.cs
@@ -1,38 +1,35 @@
 ﻿using HarmonyLib;
 using IPA;
-using IPA.Config;
-using IPA.Config.Stores;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 using IPALogger = IPA.Logging.Logger;
 
-namespace FailButton {
-	[Plugin(RuntimeOptions.SingleStartInit)]
-	public class Plugin {
-		internal static Plugin Instance { get; private set; }
-		internal static IPALogger Log { get; private set; }
-		internal static Harmony harmony { get; private set; }
+namespace FailButton
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+        internal static Harmony harmony { get; private set; }
 
-		[Init]
-		public void Init(IPALogger logger) {
-			Instance = this;
-			Log = logger;
-		}
+        [Init]
+        public void Init(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+        }
 
-		[OnStart]
-		public void OnApplicationStart() {
-			harmony = new Harmony("Kinsi55.BeatSaber.FailButton");
-			harmony.PatchAll(Assembly.GetExecutingAssembly());
-		}
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            harmony = new Harmony("Kinsi55.BeatSaber.FailButton");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
 
-		[OnExit]
-		public void OnApplicationQuit() {
-			harmony.UnpatchSelf();
-		}
-	}
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+            harmony.UnpatchSelf();
+        }
+    }
 }
